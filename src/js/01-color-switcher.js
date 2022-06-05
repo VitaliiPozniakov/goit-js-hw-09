a60263a44d04d046ef50394c68dead3b7cbf848c
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-05 16:53:08 +0300

    complete task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  startBtn: document.querySelector(`button[data-start]`),
+  stopBtn: document.querySelector(`button[data-stop]`),
+};
+
+refs.startBtn.addEventListener(`click`, onStartBtnClick);
+refs.stopBtn.addEventListener(`click`, onStopBtnClick);
+
+let hasSubscribed = false;
+let intervalId = null;
+
+function onStartBtnClick(e) {
+  if (!hasSubscribed) {
+    intervalId = setInterval(changeBgColor, 1000);
+    refs.startBtn.setAttribute('disabled', '');
+
+    refs.stopBtn.removeAttribute('disabled', '');
+    hasSubscribed = true;
+  }
+}
+
+function onStopBtnClick(e) {
+  clearInterval(intervalId);
+  refs.stopBtn.setAttribute('disabled', '');
+
+  refs.startBtn.removeAttribute('disabled', '');
+
+  hasSubscribed = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
 
