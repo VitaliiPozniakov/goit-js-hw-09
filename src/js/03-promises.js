9edd3b050551560d7931ad5c33a3d718c674eca3
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-07 10:56:47 +0300

    add draft of task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,77 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+const refs = {
+  form: document.querySelector(`.form`),
+  delayField: document.querySelector(`input[name="delay"]`),
+  stepField: document.querySelector(`input[name="step"]`),
+  amountField: document.querySelector(`input[name="amount"]`),
+  btnSubmit:  document.querySelector(`button[type="submit"]`),
+}
+
+// console.log(refs.stepField)
+// console.log(Number(refs.delayField.value) )
+// console.log(refs.stepField.value)
+// let position = 0
+
+
+// console.log(delay)
+
+
+refs.form.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(e) {
+e.preventDefault()
+
+let counter = 0
+let nextStepCf = 0
+const timerId = setInterval(() => {
+  counter += 1
+  // console.log(counter)
+  if (counter === Number(refs.amountField.value)){
+    clearInterval(timerId)
+  }
+
+  const delay = Number(refs.delayField.value ) + nextStepCf * Number(refs.stepField.value) 
+nextStepCf += 1
+  // console.log(counter)
+// console.log(delay)
+
+  createPromise(counter, delay)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+  // console.log(`kff`)
+}
+, Number(refs.delayField.value ))
+
+
+
+  // console.log(refs.delayField.value)
+  // console.log(refs.stepField.value)
+  // console.log(refs.amountField.value)
+
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });
+  
+
 }
